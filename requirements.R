87957178709f6805b7806ae9204d2b191240819e
Ibrahim <98415538+AdebayoIbrahim@users.noreply.github.com>
2025-09-07 16:46:04 +0100

    added requirements

---
+++ requirements.R
@@ -0,0 +1,17 @@
+# Install Required Packages
+install.packages("readr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("lubridate")
+install.packages("corrplot")
+install.packages("forecast")
+install.packages("tidyr")
+
+# Load Libraries
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(corrplot)
+library(forecast)
+library(tidyr)
